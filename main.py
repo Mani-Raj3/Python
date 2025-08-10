cb164003cf3013271886b97a029796757b899244
Mani-Raj3 <maniraj31102@gmail.com>
2025-08-11 01:13:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+"""print("mani is a good boy")
+print("kya haal hai")"""
+
+
+"Variables..."
+
+"""SheryansSchool = "Students"
+
+sheryansSchool = "Students"
+
+Sheryans_School = "Students"
+"""
+
+"""DataTypes"""
+
+#numbers= integer float complex
+"""a = 34
+b = 85.4   o/p-->> <class 'float'>
+print(type(b))"""  
+
+
+#string 
+"""st = '121234542 dsa'
+print(type(st))   o/p-->> <class 'str'>
+"""
+
+#boolean
+"""a = True
+b = False
+print(type(a))"""
+
+"""Strings"""
+
+# a = "SHER CODER"    # yaha pe String indexing ho rhi h  isme v indexing 0 se start hoti h 0,1,2,3,..
+# print(a[1])    # jaise yaha [1] pe H likha h to H print hoga kyuki h ka eindex 1 hai
+
+# String Slicing
+
+# a = "SHER CODER"
+# print(a[5::])  o/p-->>CODER 
+
+
+"""TypeConversion"""
+
+# a = 12
+# print(12/4)
+
+# name = "Mani"
+ # age = 21
+# print("hello my name is ", name, " and my age is ", age)     # ye simple trika h
+# print(f"my name is {name} and my age is {age}")     # print statement using a formatted string
+
+"""Input and Output """
+
+# age = input("hello what is your age ")
+
+# print(age)
+
+"""Operators"""
+
+# a = 5
+# b = 20
+# print(a+b)
+# print(b-a)
+# print(a*b)
+# print(b/a)   # yaha divide kr rhe hai
+# print(b//a)  # double slash lgane ke baad float me nhi exact divide ka nmber btyga jaise 21//4=5 
+# print(5**3)   # yaha power kr rhe h
+# print(32%5)  # mod me -> Reminder ka value aata h
+# print(4*5+2)  
+
+"""Assignment Operator"""
+
+# compound assignment operator
+
+# a = 20
+# a = a + 20  # isko a +=20 bhi likh skte h
+# a = a + 40   # isko a +=40 aise bhi likh skte h
+# a = a + 60
+# print(a)
+
+"""Comparison operator"""
+
+# a = 12.1
+# b = 12
+
+# print(a==b)  Equal to Operator
+
+# print(a!=b)
+
+# print(a > b)
+# print( 45 < 67)
+
+# print( 23 >= 23)
+
+# print( 23 <= 23)
+
+# print(ord("A"))
+# print(ord("B"))
+
+# print("A" > "B")
+
+"""Logical Operator"""
+
+# print(123>100  and 34 == 34 and 45 < 90 and ) 
+# -> "and" me dono condition(kitna v Condition de skte h apne hiasb se 2, 3, 4, kitna bhi) True hone chahiye agr ek v false hua to Result false ho jyga
+
+# print(12 != 12 or 23 == 45 or 67 == 56)
+#"or"  -> wale case me ek v condition True hua to Result True ho jyga
+
+# print(not 12 == 12)
+# not -> Reverse the boolean value
+
+print("Helllo World")
