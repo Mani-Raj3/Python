90d40aabf26e84b7c5cf43a688f9dbfcee969777
Mani-Raj3 <maniraj31102@gmail.com>
2025-08-13 23:20:57 +0530

    Add files via upload

---
+++ ConitionalStatement.py
@@ -0,0 +1,57 @@
+# a = 12
+# if( a > 10):
+#     print("I will do task A ")
+
+# else:
+#     print("I will do task B") 
+
+# money = int(input("Please provide me the money :-"))
+
+# if money == 10:
+#     print("I will have a choco bar icecream")
+
+# elif money == 20:
+#     print("I will have a mangodolly")
+
+# else:
+#     print("I will have a cone")
+
+# a = 5
+# if(a>2):
+#     print("greater than 2")
+# elif(a > 3):
+#     print("Greater than 3")  
+# elif(a > 4):
+#      print("Greater than 3")
+
+# age = 14
+# if(age>=18):
+#     print("You can vote")  Indentation ka mtlb Proper Spacing hota h
+# else:
+#     print("You can not vote")  Indentation  
+
+# marks =int (input("Enter student marks: "))
+
+# if(marks >=90):
+#     grade = "A"
+# elif(marks >=80):
+#          grade = "B"
+# elif(marks >=70):
+#     grade = "C"
+# else:
+#      grade = "D"
+
+# print("Grade of the Student ->", grade)                      
+
+
+"""Nesting """
+
+age = 95
+  #Nesting kr rhe h yaha pe
+if(age >= 18):
+    if(age >=86):
+        print("Cannot drive")
+    else:
+        print("can drive")
+else:
+    print("cannot drive")        
