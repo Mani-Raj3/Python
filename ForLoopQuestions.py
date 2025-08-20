aad71259ed0a26dc90cf609486d7855f43c7b862
Mani-Raj3 <maniraj31102@gmail.com>
2025-08-21 01:15:42 +0530

    Add files via upload

---
+++ ForLoopQuestions.py
@@ -0,0 +1,36 @@
+""" # Some For Loop Questions"""
+
+# s = ["Mani", "for", "Mani"]
+
+# # using for loop with string
+# for i in s:
+#     print(i)
+
+# s = "Mani"
+# for i in s:
+#     print(i)
+
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+# for i in range(1, 4):
+#     print(i)
+# else:  # Executed because no break in for
+#     print("No Break\n")
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(i, j)
+
+"""Write a Program to Print first 10 natural number"""
+
+# for i in range(1,11,1):
+#     print(i)
+
+"""Write a Program to Print first 10 even number"""
+
+for i in range(2,22,2):
+      print(i)
+
+   
